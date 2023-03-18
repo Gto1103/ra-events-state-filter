1ef65767627d3c205ed6cf81505eb6bf214c3f00
Vladislav <gto1103@gmail.com>
2023-03-19 00:48:44 +0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Portfolio from './components/Portfolio';
-import { data } from './components/data'
+import { data } from './components/data';
 
 function App() {
   const filters = ["All", "Websites", "Flayers", "Business Cards"];
   
   return (
     <Portfolio
-      projects={data}
-		filters={filters}
+      projects = {data}
+		filters = {filters}
     />
   );
 }
